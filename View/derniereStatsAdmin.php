a8412578c73f821bfd0b6f730b2b23fda56eb97d
DylanG-Dev <gametdylan@gmail.com>
2025-05-18 17:45:03 +0200

    Update derniereStatsAdmin.php
    Maj derniereStatsAdmin

--- View/derniereStatsAdmin.php
+++ View/derniereStatsAdmin.php
@@ -1,11 +1,12 @@
-<div id="displayFlex"><div class="container-fluid">
-        <div class="row vh-100">
+<div id="topPage"class="d-flex flex-column min-vh-200">
+    <div class="container-fluid flex-grow-1">
+        <div class="row">
             <div id="side-bar" class="col-12 col-md-3 col-xl-2 bg-dark text-white p-0 d-flex flex-column">
                 <nav class="navbar bg-dark border-bottom border-white">
                     <div class="container-fluid">
-                        <a class="navbar-brand text-white" href="index.php?deco">
+                        <a id="deco" class="navbar-brand text-white" href="index.php?deco">
                             <i class="bi bi-house-door"></i>
-                            <span class="ms-2">Déconnexion</span>
+                            <span id="boutondeco" class="ms-2"><strong>Déconnexion</strong></span>
                         </a>
                     </div>
                 </nav>
@@ -37,58 +38,42 @@
                 </nav>
             </div>
             <div class="col-12 col-md-9 col-xl-10 bg-white">
-                <h2><?php echo htmlspecialchars($_SESSION['loueur_nom']) ?></h2>
-                <h1>Rechercher les Logs du Loueur</h1>
-                <form method="post" action="index.php?derniereStatsAdmin">
-                    <table class="table">
+                <header id="head">
+                    <h2 class="alert alert-warning"><?php echo htmlspecialchars($_SESSION['loueur_nom']) ?></h2>
+                </header>
+                <h4 id="center">Dernières statistiques</h4>
+                <?php if (!empty($log)): ?>
+                    <table id="derniereStatsLoueur">
+                        <thead>
                         <tr>
-                            <td colspan="3">
-                                <input type="text" name="id" placeholder="Nom du loueur" class="form-control" />
-                            </td>
+                            <th>ID</th>
+                            <th>nom</th>
+                            <th>Date</th>
+                            <th>Erreur KO</th>
+                            <th>Erreur Timeout</th>
                         </tr>
-                        <tr>
-                            <td><br><input class="btn btn-warning" name="btnErase" type="reset" value="Effacer" /></td>
-                            <td><br><input class="btn btn-primary" name="btnRecherche" type="submit" value="Chercher" /></td>
-                        </tr>
-                    </table>
-                </form>
-
-                <?php if (isset($_POST['btnRecherche'])): ?>
-                    <?php if (!empty($logs)): ?>
-                        <h2>Résultats de la recherche</h2>
-                        <table class="table table-striped">
-                            <thead>
+                        </thead>
+                        <tbody>
+                        <?php foreach ($log as $log): ?>
                             <tr>
-                                <th>Nom</th>
-                                <th>Date</th>
-                                <th>Erreur KO</th>
-                                <th>Erreur Timeout</th>
+                                <td><?= htmlspecialchars((string) $log['idLoueur']) ?></td>
+                                <td><?= htmlspecialchars((string) $log['nom']) ?></td>
+                                <td><?= htmlspecialchars((string) $log['date']) ?></td>
+                                <td><?= htmlspecialchars((string) $log['erreurKO']) ?></td>
+                                <td><?= htmlspecialchars((string) $log['erreurTimeouts']) ?></td>
                             </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($logs as $log): ?>
-                                <tr>
-                                    <td><?= htmlspecialchars((string) $log['nom']) ?></td>
-                                    <td><?= htmlspecialchars((string) $log['date']) ?></td>
-                                    <td><?= htmlspecialchars((string) $log['appelsKO']) ?></td>
-                                    <td><?= htmlspecialchars((string) $log['timeouts']) ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    <?php else: ?>
-                        <p>Aucun log trouvé pour ce loueur.</p>
-                    <?php endif; ?>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                <?php else: ?>
+                    <p>Aucune donnée à afficher.</p>
                 <?php endif; ?>
             </div>
         </div>
     </div>
+    <footer class="text-center">
+        <h2 id="footer" class="alert alert-warning">Giurgiuman Alexandre, Barthelemy Maxence, Gamet Dylan</h2>
+    </footer>
 </div>
 
-
-<footer class="text-center mt-4">
-    <strong>Giurgiuman Alexandre, Barthelemy Maxence, Gamet Dylan</strong>
-</footer>
-
-<!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
